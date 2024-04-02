98d992f9c44cafcc34dd0a6fe6decc832d326e8f
MateusFBL <mateusdtscience@gmail.com>
2024-04-02 13:33:05 -0300

    Add classe cliente

--- codigo/src/main/java/letsburn/Cliente.java
+++ codigo/src/main/java/letsburn/Cliente.java
@@ -1,4 +1,28 @@
+
 package letsburn;
 
+/* Criação da Classe Cliente
+-id=int
+-nome=string */
+
 public class Cliente {
+    private static int proximoId = 1;
+    private int id;
+    private String nome;
+
+
+    public Cliente(String nome) {
+        this.id = proximoId++;
+        this.nome = nome;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
 }
