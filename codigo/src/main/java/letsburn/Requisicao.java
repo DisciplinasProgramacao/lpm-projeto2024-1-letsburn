b8409535b1cfbee90b17b0b8740145f360efa875
gabrieltinoco <gabriel.taguiar@gmail.com>
2024-04-10 20:51:22 -0300

    Retirar setter

--- codigo/src/main/java/letsburn/Requisicao.java
+++ codigo/src/main/java/letsburn/Requisicao.java
@@ -50,23 +50,11 @@ public class Requisicao {
         return id;
     }
     /**
-     * Metodo setter para o atributo Id. Utiliza como parametro o novo valor de Id da requisição.
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-    /**
      * Metodo Getter para o atributo horarioEntrada.
      * @return retorna o valor do horário de entrada do cliente.
      */
     public LocalDateTime getHorarioEntrada() {
         return horarioEntrada;
-    }
-    /**
-     * Metodo setter para o atributo horarioEntrada. Utiliza como parametro o novo valor de horarioEntrada.
-     */
-    public void setHorarioEntrada(LocalDateTime horarioEntrada) {
-        this.horarioEntrada = horarioEntrada;
     }
     /**
      * Metodo Getter para o atributo horarioSaida.
@@ -76,23 +64,11 @@ public class Requisicao {
         return horarioSaida;
     }
     /**
-     * Metodo setter para o atributo horarioSaida. Utiliza como parametro o novo valor de horarioSaida.
-     */
-    public void setHorarioSaida(LocalDateTime horarioSaida) {
-        this.horarioSaida = horarioSaida;
-    }
-    /**
      * Metodo Getter para o atributo qtdPessoas.
      * @return retorna o valor da quantidade de pessoas para a requisição.
      */
     public int getQtdPessoas() {
         return qtdPessoas;
-    }
-    /**
-     * Metodo setter para o atributo qtdPessoas. Utiliza como parametro o novo valor de qtdPessoas.
-     */
-    public void setQtdPessoas(int qtdPessoas) {
-        this.qtdPessoas = qtdPessoas;
     }
     /**
      * Metodo Getter para o atributo Mesa.
@@ -101,24 +77,13 @@ public class Requisicao {
     public Mesa getMesa() {
         return mesa;
     }
-    /**
-     * Metodo setter para o atributo mesa. Utiliza como parametro o novo valor de mesa.
-     */
-    public void setMesa(Mesa mesa) {
-        this.mesa = mesa;
-    }
+
     /**
      * Metodo Getter para o atributo cliente
      * @return retorna o valor do objeto cliente.
      */
     public Cliente getCliente() {
         return cliente;
-    }
-    /**
-     * Metodo setter para o atributo cliente. Utiliza como parametro o novo valor de cliente.
-     */
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
     }
 
 }
