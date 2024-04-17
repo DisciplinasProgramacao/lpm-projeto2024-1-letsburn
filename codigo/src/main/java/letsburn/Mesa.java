a4b6c107a4c48a82867332ae36e5ed3670348604
isaac_portela <kroosbr278@gmail.com>
2024-04-17 01:18:00 -0300

    fix: ajuste validacao contrutor de mesa

--- codigo/src/main/java/letsburn/Mesa.java
+++ codigo/src/main/java/letsburn/Mesa.java
@@ -5,52 +5,33 @@ package letsburn;
  */
 public class Mesa {
 
-    private String id;         // Identificador da mesa
+    private static int proximoId = 1;
+    private int id;         // Identificador da mesa
     private int capacidade;    // Capacidade máxima da mesa
     private boolean ocupada;   // Indica se a mesa está ocupada ou não
 
-    /**
-     * Construtor da classe Mesa.
-     * @param id Identificador da mesa.
-     * @param capacidade Capacidade máxima da mesa.
-     * @param ocupada Indica se a mesa está ocupada.
-     */
-    public Mesa(String id, int capacidade, boolean ocupada) {
-        this.id = id;
-        this.capacidade = capacidade;
-        this.ocupada = ocupada;
+
+    public Mesa(int capacidade) {
+        this.id = proximoId++;
+        this.ocupada = false;
+
+        if (capacidade <= 0)
+            this.capacidade = 4;
+        else if (capacidade > 8)
+            this.capacidade = 8;
+        else
+            this.capacidade = capacidade;
     }
 
-    /**
-     * Getter para o identificador da mesa.
-     * @return O identificador da mesa.
-     */
-    public String getId() {
+    public int getId() {
         return id;
     }
-
-    /**
-     * Setter para o identificador da mesa.
-     * @param id O novo identificador da mesa.
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-
     /**
      * Getter para a capacidade da mesa.
      * @return A capacidade máxima da mesa.
      */
     public int getCapacidade() {
         return capacidade;
-    }
-
-    /**
-     * Setter para a capacidade da mesa.
-     * @param capacidade A nova capacidade máxima da mesa.
-     */
-    public void setCapacidade(int capacidade) {
-        this.capacidade = capacidade;
     }
 
     /**
