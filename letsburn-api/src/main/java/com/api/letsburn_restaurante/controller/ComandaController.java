dbecbea94bd8b97c179989791a123c7560eab861
JorgeMLopes <jorge.matheus@outlook.com>
2024-06-19 12:22:30 -0300

    Criação do controller Comanda

---
+++ letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/ComandaController.java
@@ -0,0 +1,66 @@
+package com.api.letsburn_restaurante.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.letsburn_restaurante.model.Comanda;
+import com.api.letsburn_restaurante.model.ItemCardapio;
+import com.api.letsburn_restaurante.repository.ComandaRepository;
+
+@RestController
+@RequestMapping("/comandas")
+public class ComandaController {
+
+    @Autowired
+    private ComandaRepository comandaRepository;
+
+    @PostMapping
+    public ResponseEntity<Comanda> criarComanda(@RequestBody Comanda comanda) {
+        Comanda novaComanda = comandaRepository.save(comanda);
+        return ResponseEntity.ok(novaComanda);
+    }
+
+    @PostMapping("/{id}/adicionar")
+    public ResponseEntity<Comanda> adicionarProduto(@PathVariable Long id, @RequestBody ItemCardapio item) {
+        Optional<Comanda> comandaOptional = comandaRepository.findById(id);
+        if (comandaOptional.isPresent()) {
+            Comanda comanda = comandaOptional.get();
+            comanda.adicionarPedido(item);
+            comandaRepository.save(comanda);
+            return ResponseEntity.ok(comanda);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{id}/fechar")
+    public ResponseEntity<String> fecharComanda(@PathVariable Long id) {
+        Optional<Comanda> comandaOptional = comandaRepository.findById(id);
+        if (comandaOptional.isPresent()) {
+            // Lógica para fechar a comanda
+            return ResponseEntity.ok("Comanda fechada com sucesso!");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/preco")
+    public ResponseEntity<Double> perguntarPreco(@PathVariable Long id) {
+        Optional<Comanda> comandaOptional = comandaRepository.findById(id);
+        if (comandaOptional.isPresent()) {
+            Comanda comanda = comandaOptional.get();
+            double precoTotal = comanda.getPedidos().stream().mapToDouble(ItemCardapio::getPreco).sum();
+            return ResponseEntity.ok(precoTotal);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
