a3e748dd2a314224c4ab6af271824e1dff59a067
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2024-06-26 20:40:13 -0300

    remove junk code

--- letsburn-api/src/main/java/com/api/letsburn_restaurante/model/ItemCombo.java
+++ letsburn-api/src/main/java/com/api/letsburn_restaurante/model/ItemCombo.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 @Entity
 public class ItemCombo extends Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     @ManyToMany
     @JoinTable(name = "item_combo", joinColumns = @JoinColumn(name = "item_combo_id"), inverseJoinColumns = @JoinColumn(name = "item_cardapio_id"))
     private final List<ItemCardapio> itens;
