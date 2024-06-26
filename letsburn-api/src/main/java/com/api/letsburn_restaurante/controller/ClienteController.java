5f460eb5e504055f02ae3c4800d7556e7e27e4b3
JorgeMLopes <jorge.matheus@outlook.com>
2024-06-26 01:11:52 -0300

    Updates_Controllers_Cliente_Comanda

---
+++ letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/ClienteController.java
@@ -0,0 +1,42 @@
+package com.api.letsburn_restaurante.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.letsburn_restaurante.model.Cliente;
+import com.api.letsburn_restaurante.repository.ClientRepository;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @PostMapping
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+        Cliente novoCliente = clientRepository.save(cliente);
+        return ResponseEntity.ok(novoCliente);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarCliente(@PathVariable Long id) {
+        Optional<Cliente> cliente = clientRepository.findById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes() {
+        List<Cliente> clientes = clientRepository.findAll();
+        return ResponseEntity.ok(clientes);
+    }
+}
--- letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/ComandaController.java
+++ letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/ComandaController.java
@@ -1,12 +1,20 @@
 package com.api.letsburn_restaurante.controller;
 
-import com.api.letsburn_restaurante.model.Comanda;
-import com.api.letsburn_restaurante.service.ComandaService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.api.letsburn_restaurante.model.Comanda;
+import com.api.letsburn_restaurante.service.ComandaService;
 
 @RestController
 @RequestMapping("/api/comandas")
@@ -45,4 +53,17 @@ public class ComandaController {
         Comanda comanda = comandaService.removerPedido(id, idItemCardapio);
         return ResponseEntity.ok(comanda);
     }
+
+    @PutMapping("/{id}/fechar")
+    public ResponseEntity<String> fecharComanda(@PathVariable Long id) {
+        comandaService.fecharComanda(id);
+        return ResponseEntity.ok("Comanda fechada com sucesso");
+    }
+
+    @GetMapping("/{id}/preco")
+    public ResponseEntity<Double> perguntarPreco(@PathVariable Long id) {
+        Comanda comanda = comandaService.buscarComanda(id);
+        return ResponseEntity.ok(comanda.calcularValorTotal());
+    }
+
 }
