e8f1fb46190da60fe74a1b206df482cd726ee9e8
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2024-06-26 18:45:13 -0300

    resolve itemcombo

--- letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/MenuFechadoController.java
+++ letsburn-api/src/main/java/com/api/letsburn_restaurante/controller/MenuFechadoController.java
@@ -2,7 +2,6 @@ package com.api.letsburn_restaurante.controller;
 
 import com.api.letsburn_restaurante.dto.AdicionarItemAvulsoDTO;
 import com.api.letsburn_restaurante.dto.ResquestMenuFechado;
-import com.api.letsburn_restaurante.factory.ItemComboFactory;
 import com.api.letsburn_restaurante.model.Comanda;
 import com.api.letsburn_restaurante.model.ItemCardapio;
 import com.api.letsburn_restaurante.model.ItemCombo;
@@ -21,67 +20,66 @@ import java.util.stream.Collectors;
 @RequestMapping("/menuFechado")
 public class MenuFechadoController {
 
-    @Autowired
-    private ItemComboRepository itemComboRepository;
+  @Autowired
+  private ItemComboRepository itemComboRepository;
 
-    @Autowired
-    private ItemCardapioRepository itemCardapioRepository;
+  @Autowired
+  private ItemCardapioRepository itemCardapioRepository;
 
-    @Autowired
-    private ComandaRepository comandaRepository;
+  @Autowired
+  private ComandaRepository comandaRepository;
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<ItemCombo> cadastrarMenuFechado(@RequestBody ResquestMenuFechado menuFechado) {
-        List<ItemCardapio> itens = menuFechado.itens().stream()
-                .map(nome -> itemCardapioRepository.findByNome(nome))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
+  @PostMapping("/cadastrar")
+  public ResponseEntity<ItemCombo> cadastrarMenuFechado(@RequestBody ResquestMenuFechado menuFechado) {
+    List<ItemCardapio> itens = menuFechado.itens().stream()
+        .map(nome -> itemCardapioRepository.findByNome(nome))
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .collect(Collectors.toList());
 
-        ItemCombo novoItemCombo = ItemComboFactory.createItemCombo("MenuFechado", menuFechado.nome(), menuFechado.preco());
-        novoItemCombo.setItens(itens);
+    ItemCombo novoItemCombo = new ItemCombo(menuFechado.nome(), menuFechado.preco(), itens);
 
-        ItemCombo itemComboSalvo = itemComboRepository.save(novoItemCombo);
-        return ResponseEntity.ok(itemComboSalvo);
+    ItemCombo itemComboSalvo = itemComboRepository.save(novoItemCombo);
+    return ResponseEntity.ok(itemComboSalvo);
+  }
+
+  @GetMapping("/listar")
+  public ResponseEntity<List<ItemCombo>> listarMenuFechado() {
+    List<ItemCombo> menusFechados = itemComboRepository.findAll();
+    return ResponseEntity.ok(menusFechados);
+  }
+
+  @GetMapping("/buscar/{id}")
+  public ResponseEntity<ItemCombo> buscarMenuFechado(@PathVariable Long id) {
+    Optional<ItemCombo> menuFechadoOpt = itemComboRepository.findById(id);
+    if (menuFechadoOpt.isPresent()) {
+      return ResponseEntity.ok(menuFechadoOpt.get());
+    } else {
+      return ResponseEntity.notFound().build();
     }
+  }
 
-    @GetMapping("/listar")
-    public ResponseEntity<List<ItemCombo>> listarMenuFechado() {
-        List<ItemCombo> menusFechados = itemComboRepository.findAll();
-        return ResponseEntity.ok(menusFechados);
+  @PostMapping("/adicionarItemAvulso")
+  public ResponseEntity<ItemCombo> adicionarItemAvulso(@RequestBody AdicionarItemAvulsoDTO adicionarItemAvulsoDTO) {
+    Long comandaId = adicionarItemAvulsoDTO.comandaId();
+    Long comboId = adicionarItemAvulsoDTO.comboId();
+    Long itemId = adicionarItemAvulsoDTO.itemId();
+
+    Optional<Comanda> comandaOpt = comandaRepository.findById(comandaId);
+    Optional<ItemCombo> comboOpt = itemComboRepository.findById(comboId);
+    Optional<ItemCardapio> itemAvulsoOpt = itemCardapioRepository.findById(itemId);
+
+    if (comandaOpt.isPresent() && comboOpt.isPresent() && itemAvulsoOpt.isPresent()) {
+      Comanda comanda = comandaOpt.get();
+      ItemCombo combo = comboOpt.get();
+      ItemCardapio itemAvulso = itemAvulsoOpt.get();
+
+      comanda.adicionarPedido(itemAvulso);
+      itemComboRepository.save(combo);
+
+      return ResponseEntity.ok(combo);
+    } else {
+      return ResponseEntity.notFound().build();
     }
-
-    @GetMapping("/buscar/{id}")
-    public ResponseEntity<ItemCombo> buscarMenuFechado(@PathVariable Long id) {
-        Optional<ItemCombo> menuFechadoOpt = itemComboRepository.findById(id);
-        if (menuFechadoOpt.isPresent()) {
-            return ResponseEntity.ok(menuFechadoOpt.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PostMapping("/adicionarItemAvulso")
-    public ResponseEntity<ItemCombo> adicionarItemAvulso(@RequestBody AdicionarItemAvulsoDTO adicionarItemAvulsoDTO) {
-        Long comandaId = adicionarItemAvulsoDTO.comandaId();
-        Long comboId = adicionarItemAvulsoDTO.comboId();
-        Long itemId = adicionarItemAvulsoDTO.itemId();
-
-        Optional<Comanda> comandaOpt = comandaRepository.findById(comandaId);
-        Optional<ItemCombo> comboOpt = itemComboRepository.findById(comboId);
-        Optional<ItemCardapio> itemAvulsoOpt = itemCardapioRepository.findById(itemId);
-
-        if (comandaOpt.isPresent() && comboOpt.isPresent() && itemAvulsoOpt.isPresent()) {
-            Comanda comanda = comandaOpt.get();
-            ItemCombo combo = comboOpt.get();
-            ItemCardapio itemAvulso = itemAvulsoOpt.get();
-
-            combo.adicionarItemAvulso(comanda, itemAvulso);
-            itemComboRepository.save(combo);
-
-            return ResponseEntity.ok(combo);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+  }
 }
